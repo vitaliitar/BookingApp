c921890b2291357f499d164022d3e6d8fec1839e
vitaliitar <vitalii.tar@uzhnu.edu.ua>
2020-09-13 17:17:50 +0300

    Add Frontend authentication

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <React.StrictMode>
+import "./index.css";
+import App from "./App";
+import makeStore from "./redux/store";
+
+const store = makeStore();
+
+const WithProvider = () => (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>
 );
+
+ReactDOM.render(<WithProvider />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+	return (
+		<div>
+			You made it🥳
+		</div>
+	)
+}
+export default Home
